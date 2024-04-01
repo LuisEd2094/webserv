0bc69397adfabca9e632ae2d34855c7678990cb1
Luis <luissoto2094@gmail.com>
2024-04-01 13:38:11 +0200

    moved bind and socket confi to it's own function

--- src/main.cpp
+++ src/main.cpp
@@ -34,15 +34,14 @@ void exitError(const std::string& error)
     std::exit(EXIT_FAILURE);
 
 }
-int main()
-{
-    t_confi confi;
 
-    std::memset(&(confi.hints), 0, sizeof(confi.hints));
-    confi.hints.ai_family = AF_UNSPEC; //takes ipv4 and ipv6
-    confi.hints.ai_socktype = SOCK_STREAM; // TCP stream sockets
-    confi.hints.ai_flags = AI_PASSIVE;
-    confi.port = "80";
+void setConfi(t_confi *confi)
+{
+    std::memset(&(confi->hints), 0, sizeof(confi->hints));
+    confi->hints.ai_family = AF_UNSPEC; //takes ipv4 and ipv6
+    confi->hints.ai_socktype = SOCK_STREAM; // TCP stream sockets
+    confi->hints.ai_flags = AI_PASSIVE;
+    confi->port = "80";
     //    If the AI_PASSIVE flag is specified in hints.ai_flags, and node
     //    is NULL, then the returned socket addresses will be suitable for
     //    bind(2)ing a socket that will accept(2) connections.  The
@@ -53,19 +52,29 @@ int main()
     //    the host's network addresses.  If node is not NULL, then the
     //    AI_PASSIVE flag is ignored.
 
-    if ((confi.status = getaddrinfo(NULL, confi.port.c_str(), &confi.hints, &confi.servinfo)) != 0) 
-        exitError("getaddrinfo error: " + static_cast<std::string>(gai_strerror(confi.status)));
+    if ((confi->status = getaddrinfo(NULL, confi->port.c_str(), &confi->hints, &confi->servinfo)) != 0) 
+        exitError("getaddrinfo error: " + static_cast<std::string>(gai_strerror(confi->status)));
     // make a socket:
 
-    confi.socketFd = socket(confi.servinfo->ai_family, confi.servinfo->ai_socktype, confi.servinfo->ai_protocol);
+    if (confi->socketFd = socket(confi->servinfo->ai_family, confi->servinfo->ai_socktype, confi->servinfo->ai_protocol) == -1)
+    {
+        freeaddrinfo(confi->servinfo);
+        exitError("socket error: couldn't create socket");
+    };
     // bind it to the port we passed in to getaddrinfo():
 
-    if (bind(confi.socketFd, confi.servinfo->ai_addr, confi.servinfo->ai_addrlen) != 0)
+
+    if (bind(confi->socketFd, confi->servinfo->ai_addr, confi->servinfo->ai_addrlen) != 0)
     {
-        freeaddrinfo(confi.servinfo);
-        exitError("bind error: couldn't bind port " + confi.port + " to socket");
+        freeaddrinfo(confi->servinfo);
+        exitError("bind error: couldn't bind port " + confi->port + " to socket");
     }
-    
+}
+int main()
+{
+    t_confi confi;
+
+    setConfi(&confi);
     freeaddrinfo(confi.servinfo);
     test();
     return 0;
