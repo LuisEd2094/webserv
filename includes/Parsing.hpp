d049928fe38d88a446a7c7c1fb5cf4beb9a9b24a
dacortes <dannyel.cortesr@gmail.com>
2024-04-06 12:12:03 +0200

    [ADD] branch parsing and parsing.hpp

---
+++ includes/Parsing.hpp
@@ -0,0 +1,41 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   parsing.hpp                                        :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: dacortes <dacortes@student.42.fr>          +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2024/04/06 12:01:39 by dacortes          #+#    #+#             */
+/*   Updated: 2024/04/06 12:07:07 by dacortes         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#ifndef PARSING_HPP
+#define PARSING_HPP
+
+/******************************************************************************/
+/*                            CLASS                                           */
+/******************************************************************************/
+
+class Parsing
+{
+	private:
+	public:
+	/*
+	 * Orthodox Canonical Form
+	*/
+	Parsing(void);
+	Parsing(const Parsing &obj);
+	Parsing &operator=(const Parsing &obj);
+	~Parsing(void);
+	/*
+	 * Get Methods
+	*/
+	/*
+	 * Membert Funtions
+	*/
+	/*
+	 * Exception Classes
+	*/
+};
+#endif
