6a8e1131509a6581456094563ba8c5066083baf5
Luis <luissoto2094@gmail.com>
2024-04-29 13:18:26 +0200

    started working on clients to handle each interaction by themselves. Overseer needs to send if their socket is being read or written to so it knows if it has to receive info

--- includes/Client.hpp
+++ includes/Client.hpp
@@ -6,10 +6,20 @@
 # include <string>
 # include <vector>
 # include <fstream>
+# include <poll.h>
+
 
 
 
 # define SEND_SIZE 5
+# define RECV_SIZE 20
+
+enum Actions {
+    WAIT,
+    GET,
+    POST, 
+    DELETE
+};
 
 class Server;
 
@@ -18,24 +28,35 @@ class Client
     public:
         Client(Server *server);
         ~Client();
-
-        int     recvData();
-        void    firstSendData(const std::string & http);
-        void    sendBatch();
-        bool    hasPending();
-
+        int     clientAction(int action);
         //getters
         int getSocket();
         class clientException;
 
 
     private:
+
+        //PLACER HOLDER UNTIL WE ADD THE PARSER
+        void                    testingParse();
+
+
+        void                    readFromFD();
+        std::size_t             _result;
+
+        const char *            _msg_to_send;
+
+
+
+
+        int                     executeGetAction();
+        int                     executePostAction();
         int                     _fd;
         bool                    _has_msg_pending;
         std::string             _msg_pending;
         std::size_t             _bytes_sent;
 
-        char                    _in_message[1024];
+        Actions                 _action;
+        char                    _in_message[RECV_SIZE];
         std::size_t             _in_size;
         std::string             _in_http;
         std::vector<void *>     _in_body;
--- src/Client/Client.cpp
+++ src/Client/Client.cpp
@@ -15,8 +15,6 @@ class Client::clientException : public std::exception
         }
 };
 
-
-
 //public:
 Client::Client(Server *server)
 {
@@ -29,9 +27,69 @@ Client::Client(Server *server)
     {
         throw Client::clientException("accept" + static_cast<std::string>(strerror(errno)));
     }
-    _has_msg_pending = true;
+    _action = WAIT;
+    _has_msg_pending = false;
     _bytes_sent = 0;
 }
+
+
+void Client::testingParse()
+{
+    std::size_t i = 0;
+    for (; i < _result ; ++i)
+    {
+        if (_in_message[i] == '\n') // found end of first line
+        {
+            if (_in_http.find("GET") != std::string::npos)
+                _action = GET;
+            else if (_in_http.find("POST") != std::string::npos)
+                _action = POST;
+            else if (_in_http.find("DELETE") != std::string::npos)
+                _action = DELETE;
+            else
+                return; //RETURN ERROR?
+            break;
+        }
+    }
+}
+
+
+void Client::readFromFD()
+{
+    _result = recv(_fd, _in_message, sizeof(_in_message), 0);
+    _in_http.append(_in_message, std::strlen(_in_message));
+}
+
+int Client::clientAction( int action )
+{
+    if (action & POLLIN)
+        readFromFD();
+    switch (_action)
+    {
+        case WAIT:
+            //EXAMPLE FOR GET
+            //SHOULD DO CLIENT RECV AND THEN CHECK WHAT ACTION IT HAS TO TAKE
+
+            // this->executeGetAction -> should parse Header.
+            // this->parseData() --> parseData should set _actions to get post delete
+            // this->clientAction() so that it executes the correct order
+
+            testingParse();
+        case GET:
+            return executeGetAction();
+            break;
+
+        case POST:
+            break;
+
+        case DELETE:
+            break;
+
+    }
+    return _result;
+}
+
+
 int Client::getSocket()
 {
     return _fd;
@@ -43,127 +101,125 @@ Client::~Client()
     close(_fd);
 }
 
-int Client::recvData()
+
+int Client::executePostAction()
 {
-    std::size_t result = recv(_fd, _in_message, sizeof(_in_message), 0);
-    if (result == -1)
-        std::cerr << "recv: " + static_cast<std::string>(strerror(errno)) << std::endl;
-    else
+        // std::size_t result = recv(_fd, _in_message, sizeof(_in_message), 0);
+    // if (result == -1)
+    //     std::cerr << "recv: " + static_cast<std::string>(strerror(errno)) << std::endl;
+    // else
+    // {
+    //     _in_http.append((char *)_in_message, result);
+
+    //     std::size_t end_http = _in_http.find("\r\n\r\n"); // Ubuntu Curl sends this at the end of http
+    //     if (end_http != std::string::npos)
+    //     {
+    //         std::cout << "HTTP HEADER:\n" << _in_http.substr(0, end_http + 4) << std::endl;
+    //         std::size_t content_length_start = _in_http.find("Content-Length: ");
+    //         if (content_length_start != std::string::npos)
+    //         {
+    //             content_length_start += std::strlen("Content-Length: ");
+    //             std::size_t content_length_end = _in_http.find("\r\n", content_length_start);
+    //             if (content_length_end != std::string::npos)
+    //             {
+    //                 std::string content_length_str = _in_http.substr(content_length_start, content_length_end - content_length_start);
+    //                 int content_length = std::atoi(content_length_str.c_str()); // check values
+    //                 if (_in_http.size() >= end_http + 4 + content_length) // 4 for "\r\n\r\n"
+    //                 {
+    //                    commenting to keep track. If no boundary found, no http extra, direct transfer
+    //                      std::size_t boundary_start = _in_http.find("boundary=");
+    //                     boundary_start += std::strlen("boundary=");
+    //                     std::size_t boundary_end = _in_http.find("\r\n", boundary_start);
+
+    //                     std::string boundary_str = _in_http.substr(boundary_start, boundary_end - boundary_start + 2);
+
+    //                     std::cout << "FOUND BOUNDARY:" << boundary_str;
+
+    //                     std::size_t other = std::strlen("Content-Disposition: form-data; name=\"image\"; filename=\"image.jpeg\"");
+    //                     other += std::strlen("Content-Type: image/jpeg");
+    //                     other = 0;
+
+    //                     std::string binary_data = _in_http.substr( other + end_http + 4 , content_length);
+    //                     std::cout << binary_data << std::endl;
+    //                     std::ofstream outfile("output_file.jpeg", std::ios::binary);
+    //                     if (outfile.is_open())
+    //                     {
+    //                         outfile.write(binary_data.c_str(), binary_data.size());
+    //                         outfile.close();
+    //                         std::cout << "Binary data written to file.\n";
+    //                     }
+    //                     else
+    //                     {
+    //                         std::cerr << "Error opening file for writing.\n";
+    //                     }
+    //                                     Clear the _in_http buffer and append any remaining data
+    //                     _in_http.clear();
+    //                     if (_in_http.size() > end_http + 4 + content_length)
+    //                     {
+    //                         _in_http.append(_in_message + end_http + 4 + content_length, _in_http.size() - (end_http + 4 + content_length));
+    //                     }
+    //                 }
+
+
+    //             }
+
+
+    //         }
+
+    //     }
+
+    // }
+    return (0);
+}
+
+int Client::executeGetAction()
+{
+    // SUPPOSE WE ALREADY KNOW THE HTTP IS DONE;
+
+    // AFTER GET IS DONE WE SHOULD CHECK AND SEE WHAT THE MESSAGE WE ARE GOING TO SEND IS.
+
+    // std::string http = "HTTP/1.1 200 OK\r\n"
+    //     "Content-Type: text/plain\r\n"
+    //     "Content-Length: 13\r\n"
+    //     "\r\n"
+    //     "Hello, world!\r\n\0";
+
+    if (_has_msg_pending == false  && _in_http.find("\r\n\r\n") != std::string::npos)
     {
-        _in_http.append((char *)_in_message, result);
-        std::size_t end_http = _in_http.find("\r\n\r\n"); // Ubuntu Curl sends this at the end of http
-        if (end_http != std::string::npos)
+        _msg_pending = "HTTP/1.1 200 OK\r\n"
+            "Content-Type: text/plain\r\n"
+            "Content-Length: 13\r\n"
+            "\r\n"
+            "Hello, world!\r\n\0";
+        _msg_to_send = _msg_pending.c_str();
+        _has_msg_pending = true;
+    }
+    if (_has_msg_pending)
+    {
+        std::size_t _result;
+        if ((_result = send(_fd, _msg_to_send + _bytes_sent, SEND_SIZE, SO_NOSIGPIPE) )== -1)
+            std::cerr << "send: " + static_cast<std::string>(strerror(errno)) << std::endl;
+        if (_result + _bytes_sent != std::strlen(_msg_to_send))
         {
-            std::cout << "HTTP HEADER:\n" << _in_http.substr(0, end_http + 4) << std::endl;
-            std::size_t content_length_start = _in_http.find("Content-Length: ");
-            if (content_length_start != std::string::npos)
-            {
-                content_length_start += std::strlen("Content-Length: ");
-                std::size_t content_length_end = _in_http.find("\r\n", content_length_start);
-                if (content_length_end != std::string::npos)
-                {
-                    std::string content_length_str = _in_http.substr(content_length_start, content_length_end - content_length_start);
-                    int content_length = std::atoi(content_length_str.c_str()); // check values
-                    if (_in_http.size() >= end_http + 4 + content_length) // 4 for "\r\n\r\n"
-                    {
-                        /*commenting to keep track. If no boundary found, no http extra, direct transfer*/
-/*                         std::size_t boundary_start = _in_http.find("boundary=");
-                        boundary_start += std::strlen("boundary=");
-                        std::size_t boundary_end = _in_http.find("\r\n", boundary_start);
-
-                        std::string boundary_str = _in_http.substr(boundary_start, boundary_end - boundary_start + 2);
-
-                        std::cout << "FOUND BOUNDARY:" << boundary_str;
- */
-                        std::size_t other = std::strlen("Content-Disposition: form-data; name=\"image\"; filename=\"image.jpeg\"");
-                        other += std::strlen("Content-Type: image/jpeg");
-                        other = 0;
-
-                        std::string binary_data = _in_http.substr( other + end_http + 4 , content_length);
-                        std::cout << binary_data << std::endl;
-                        std::ofstream outfile("output_file.jpeg", std::ios::binary);
-                        if (outfile.is_open())
-                        {
-                            outfile.write(binary_data.c_str(), binary_data.size());
-                            outfile.close();
-                            std::cout << "Binary data written to file.\n";
-                        }
-                        else
-                        {
-                            std::cerr << "Error opening file for writing.\n";
-                        }
-                                        // Clear the _in_http buffer and append any remaining data
-                        _in_http.clear();
-                        if (_in_http.size() > end_http + 4 + content_length)
-                        {
-                            _in_http.append(_in_message + end_http + 4 + content_length, _in_http.size() - (end_http + 4 + content_length));
-                        }
-                    }
-
-
-                }
-
-
-            }
+            _bytes_sent += _result;
+        }
+        else
+        {
+            _has_msg_pending = false;
+            return (0);
 
         }
+    }
 
-    }
-    if (result == 0)
-    {
-        //std::cout << _in_http << std::endl;
-    }
-    return (result);
+
+
+
+
+
+
+
+    return (1);
 }
-
-bool    Client::hasPending()
-{
-    return _has_msg_pending;
-}
-
-
-#include <sys/types.h>
-#include <sys/socket.h>
-
-//MSG_NOSIGNAL NO PIPE  FOR LINUX 
-/// SO_NOSIGPIPE FOR MAC
-void Client::sendBatch()
-{
-    const char *msg_to_send = _msg_pending.c_str();
-
-
-    std::size_t result;
-    if ((result = send(_fd, msg_to_send + _bytes_sent, SEND_SIZE, SO_NOSIGPIPE)) == -1)
-        std::cerr << "send: " + static_cast<std::string>(strerror(errno)) << std::endl;
-    else if (result + _bytes_sent != std::strlen(msg_to_send))
-    {
-        _bytes_sent += result;
-    }
-    else
-    {
-        _has_msg_pending = false;
-        //close connection?
-    }   
-}
-
-//std::strlen(http.c_str())
-void Client::firstSendData(const std::string& http)
-{
-    const char *msg_to_send = http.c_str();
-    std::size_t result;
-    if ((result = send(_fd, msg_to_send, SEND_SIZE, SO_NOSIGPIPE) )== -1)
-        std::cerr << "send: " + static_cast<std::string>(strerror(errno)) << std::endl;
-    if (result != std::strlen(msg_to_send))
-    {
-        _msg_pending = http;
-        _bytes_sent = result;
-    }
-    else
-    {
-        //close connection?
-    }
-}
-
 
 //private:
 Client::Client () {}
