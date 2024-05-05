41aabe2096e7f6c74b990a123d6ced4554f1893a
Luis <luissoto2094@gmail.com>
2024-05-05 09:46:18 +0200

    added CGI test files

---
+++ CGI/nolen.py
@@ -0,0 +1,17 @@
+print("HTTP/1.1 200 OK")
+print("Content-Type: text/html")
+print("")
+
+# HTML content to be displayed in the browser
+print("""
+<!DOCTYPE html>
+<html>
+<head>
+    <title>CGI Test</title>
+</head>
+<body>
+    <h1>CGI Test Page</h1>
+    <p>This is a simple CGI test.</p>
+</body>
+</html>
+""")
