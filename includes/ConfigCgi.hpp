f71d143068d66281e54376b996e62646a0cf46c7
dacortes <87203430+dacortes@users.noreply.github.com>
2024-06-18 14:00:38 +0000

    [IMP] class ConfigCgi

--- includes/ConfigCgi.hpp
+++ includes/ConfigCgi.hpp
@@ -6,27 +6,39 @@ class ConfigCgi;
 # include "ConfigElement.hpp"
 # include "ParsingServer.hpp"
 # include "Parsing.hpp"
+# include "Path.hpp"
 
 # include "Client.hpp"
 
 class ConfigCgi: public ConfigElement
 {
 	private:
-		std::string					__elemType__;
-		std::string					__elemArgument__;
+		std::string							__elemType__;
+		std::string							__elemArgument__;
+		std::string							extension;
+		std::map<std::string, std::string>	metaVar;
+		Path								execute;
 
 	public:
 
 		ConfigCgi(void); // TODO
 		ConfigCgi(ParsingCgi& obj);
 		ConfigCgi(const ConfigCgi& obj);
-		ConfigCgi &operator=( const ConfigCgi& obj){(void)obj; return (*this);};
+		ConfigCgi &operator=( const ConfigCgi& obj);
 		~ConfigCgi(){};
 
 		void set__elemType__(std::string v){this->__elemArgument__ = v;};
 		void set__elemArgument__(std::string v){this->__elemArgument__ = v;};
+		void setExtension(std::string &extension);
+		void setMetaVar(std::string &metaVar);
+		void setExecute(std::string &execute);
+
+
 		std::string get__elemType__(void) const {return this->__elemArgument__;};
 		std::string get__elemArgument__(void) const {return this->__elemArgument__;};
+		std::string getExtension(void) const;
+		Path		getExecute(void) const;
+		const std::map<std::string, std::string> &getMetaVar(void) const;
 
 		bool prepareClient4ResponseGeneration(Client& client);
 
--- src/Config/ConfigCgi.cpp
+++ src/Config/ConfigCgi.cpp
@@ -11,20 +11,12 @@ ConfigCgi::ConfigCgi(ParsingCgi& obj)
     {
         this->parseKeyVal(cgi->first, cgi->second);
     }
+    this->extension = this->get__elemArgument__();
 }
 
 ConfigCgi::ConfigCgi(const ConfigCgi& obj)
 {
-    this->__elemArgument__ = obj.__elemArgument__;
-    this->__elemType__ = obj.__elemType__;
-}
-
-void ConfigCgi::recursivePrint(int recursiveLvl)
-{
-	std::cout << ConfigElement::genSpace(recursiveLvl) ;
-    recursiveLvl++;
-    std::cout << "- CGI: " << this->__elemArgument__ << std::endl;
-
+    *this = obj;
 }
 
 bool ConfigCgi::prepareClient4ResponseGeneration(Client& client)
@@ -38,10 +30,13 @@ void ConfigCgi::parseKeyVal(std::string key, std::string val)
     if (key == "__elemType__")
         this->__elemType__ = val;
     else if (key == "__elemArgument__")
-    {
         this->__elemArgument__ = val;
-    }
-    else if (key == "smth");
+    else if (key == "extension:")
+        this->setExtension(val);
+    else if (key == "metaVar")
+        this->setMetaVar(val);
+    else if (key == "execute")
+        this->setExecute(val);
     else
 		throw ParamError(std::string("Error: cgi key not found.") + std::string(" key:") + key);
 }
@@ -52,9 +47,89 @@ void ConfigCgi::setDefaults()
     this->__elemArgument__ = "";
 }
 
+void ConfigCgi::setExtension(std::string &extension)
+{
+    this->extension = extension;
+
+}
+
+void ConfigCgi::setMetaVar(std::string &metaVar)
+{
+    std::list<std::string> split = ft_split(metaVar, ' ');
+    std::list<std::string>::iterator line = split.begin();
+    for (; line != split.end(); line++)
+    {
+        std::list<std::string> tmp = ft_split(*line, '=');
+        this->metaVar[tmp.front()] = tmp.back();
+    }
+}
+
+void ConfigCgi::setExecute(std::string &execute)
+{
+    this->execute = execute;
+}
+
+std::string ConfigCgi::getExtension(void) const
+{
+    return (this->extension);
+}
+
+Path ConfigCgi::getExecute(void) const
+{
+    return (this->execute);
+}
+
+const std::map<std::string, std::string> &ConfigCgi::getMetaVar(void) const
+{
+    return (this->metaVar);
+}
+
+void ConfigCgi::recursivePrint(int recursiveLvl)
+{
+
+	std::cout << ConfigElement::genSpace(recursiveLvl) ;
+	std::cout << "CGI (" << this->get__elemArgument__() << ")" << std::endl;
+    recursiveLvl++;
+    std::cout << ConfigElement::genSpace(recursiveLvl) ;
+	std::cout << "extension: " << this->getExtension() << std::endl;
+
+    std::cout << ConfigElement::genSpace(recursiveLvl) ;
+	std::cout << "execute: " << this->getExecute() << std::endl;
+    std::cout << ConfigElement::genSpace(recursiveLvl) ;
+    std::cout << "metaVars:";
+    recursiveLvl++;
+    std::map<std::string, std::string>::const_iterator metaVar = this->getMetaVar().begin();
+
+    std::cout << "len: " << this->getMetaVar().size()<< std::endl;
+    for (; metaVar != this->getMetaVar().end(); metaVar++)
+    {
+        std::cout << ConfigElement::genSpace(recursiveLvl) ;
+        std::cout << metaVar->first << ": " << metaVar->second << std::endl;
+    }
+}
+
+ConfigCgi &ConfigCgi::operator=( const ConfigCgi& obj)
+{
+    this->__elemType__ = obj.__elemType__;
+    this->__elemArgument__ = obj.__elemArgument__;
+    this->extension = obj.extension;
+    this->metaVar = obj.metaVar;
+    this->execute = obj.execute;
+    return (*this);
+}
+
 std::ostream &operator<<(std::ostream &os, const ConfigCgi &cgi)
 {
     os << "CGI: " << cgi.get__elemArgument__() ;
+    os << "extension: " << cgi.getExtension();
+    os << "execute: " << cgi.getExecute();
+    cgi.getMetaVar();
+    std::map<std::string, std::string>::const_iterator metaVar = cgi.getMetaVar().begin();
+    os << "metaVar:";
+    for (; metaVar != cgi.getMetaVar().end(); metaVar++)
+        os << " " << metaVar->first << ": " << metaVar->second;
     return (os);
 }
+
+
 
